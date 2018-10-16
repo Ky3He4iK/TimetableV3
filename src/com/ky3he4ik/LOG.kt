ec3124d6b736d0a1174dee31eb0a04bc626c16a4
ky3he4ik <misha10311@yandex.ru>
2018-10-16 18:32:30 +0500

    reformatted IO.kt

--- src/com/ky3he4ik/LOG.kt
+++ src/com/ky3he4ik/LOG.kt
@@ -3,14 +3,26 @@ package com.ky3he4ik
 internal object LOG {
     private const val logBoundaryOpen = "------LOG PART------"
     private const val logBoundaryClose = "____END LOG PART____"
+    private const val esc = "\u001B"
+    private const val bold = "[1;"
+    private const val normal = "[0;"
+    private const val endEsc = "m"
 
-    enum class LogLevel(val lvl: Int) {
-        VERBOSE(0),
-        DEBUG(3),
-        INFO(6),
-        WARNING(9),
-        ERROR(12),
-        SILENT(100)
+    enum class LogLevel(val lvl: Int, val color: Color) {
+        VERBOSE(0, Color.BLACK),
+        DEBUG(3, Color.GRAY),
+        INFO(6, Color.WHITE),
+        WARNING(9, Color.MAGENTA),
+        ERROR(12, Color.RED),
+        SILENT(100, Color.RED)
+    }
+
+    enum class Color(val code: String, val isBold: Boolean) {
+        RED("31", true),
+        MAGENTA("35", true),
+        WHITE("30", false),
+        GRAY("38", false),
+        BLACK("37", false)
     }
 
     var logLevel = LogLevel.INFO
@@ -21,7 +33,6 @@ internal object LOG {
         Thread.sleep(10)
         println("\n$logBoundaryClose")
         //TODO: timestamp in logs
-        //TODO: colored messages
     }
 
     fun e(tag: String, message: String) = write(tag, message, LogLevel.ERROR)
@@ -38,6 +49,10 @@ internal object LOG {
 
     private fun write(tag: String, message: String, level: LogLevel) {
         if (level.lvl >= logLevel.lvl)
-            println("$logBoundaryOpen\n$tag: $message\n$logBoundaryClose")
+            println("${LOG.toColoredText(logBoundaryOpen, logLevel.color)}\n$tag: $message\n" +
+                    LOG.toColoredText(logBoundaryClose, logLevel.color))
     }
+
+    private fun toColoredText(text: String, color: Color): String
+            = "$esc${if (color.isBold) bold else normal}${color.code}$endEsc$text"
 }
